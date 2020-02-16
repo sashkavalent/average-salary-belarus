2eed8406c961c83e4161cdfd89ba5a6f40d5a9e2
Alexander Ryhlitsky <alexander.rykhlitskiy@gmail.com>
2020-02-16 12:21:58 +0300

    Add fresh xlsx

--- salary_history/excel_table.rb
+++ salary_history/excel_table.rb
@@ -4,7 +4,8 @@ require 'creek'
 class SalaryHistory::ExcelTable < SalaryHistory::BaseTable
   # Link is from https://www.belstat.gov.by/ofitsialnaya-statistika/realny-sector-ekonomiki/stoimost-rabochey-sily/operativnye-dannye/nominalnaya-nachislennaya-srednemesyachnaya-zarabotnaya-plata-rabotnikov-respubliki-belarus-po-kvart/vidam-ekonomicheskoy-deyatelnosti-po-kvartalam-2018-g/
   # "Номинальная начисленная средняя заработная плата работников Республики Беларусь с 1991 по 2019 гг. (.xls)"
-  URL = 'https://www.belstat.gov.by/upload-belstat/upload-belstat-excel/Oficial_statistika/2019/nach_sr_zarplata-91-19_1911.xlsx'.freeze
+  URL = 'https://www.belstat.gov.by/upload-belstat/upload-belstat-excel/Oficial_statistika/2019/nach_sr_zarplata-91-19_1912.xlsx'.freeze
+       # https://www.belstat.gov.by/upload-belstat/upload-belstat-excel/Oficial_statistika/2019/nach_sr_zarplata-91-19_1911.xlsx
 
   def start_date
     Date.parse('01.01.1991')
@@ -13,15 +14,17 @@ class SalaryHistory::ExcelTable < SalaryHistory::BaseTable
   protected
 
   def fetch_salaries_grouped_by_year
-    puts "request to #{URL}}"
     path = 'salaries/2019.xlsx'
     CachedRequest.run(URL, path)
     creek = Creek::Book.new "cache/#{path}"
     sheet = creek.sheets[0]
-    rows = sheet.rows.to_a[4..32].map { |row| row.values[1..] }
+    # rows = sheet.rows.to_a[4..32].map { |row| row.values[1..] }
+    rows = sheet.rows.to_a[4..32].map { |row| row.values }
 
     rows.each_with_object({}) do |row, obj|
-      obj[row[0][0..3].to_i] = row[1..].map(&:to_i)
+      year = row[0][0..3].to_i
+      salaries_per_months = row[1..].map(&:to_i)
+      obj[year] = salaries_per_months
     end
   end
 end
